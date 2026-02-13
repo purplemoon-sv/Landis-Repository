74f5910d92b5dbd7e2c3eae7700126fb615674a8
Purplmoon <289704@burlcoschools.org>
2026-02-13 15:03:03 +0000

    modified:   MilesPerGallon.cpp

--- MilesPerGallon.cpp
+++ MilesPerGallon.cpp
@@ -5,7 +5,10 @@ int main()
 {
     int gallons = 15;
     int milesLeft = 375;
+    //Data for calculation
     int milesPerGallon = milesLeft / gallons;
+    //Math
     cout << "The amount of gallons your car uses per mile is " << milesPerGallon;
+    //Shown to the User
     return 0;
 }
